9c742cf0846997cec3e633063b3affa0643c3c77
craftidev <ivann.codev@gmail.com>
2023-11-29 11:13:52 +0100

    contacts page

--- crudnext13/app/notes/page.tsx
+++ crudnext13/app/notes/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import PocketBase from 'pocketbase';
-import styles from './Notes.module.css';
-import CreateNote from "./[id]/CreateNote";
+import styles from './Contacts.module.css';
+import CreateContact from "./[id]/CreateContact";
 
 // Next13 export variables for cache behavior
 export const dynamic = 'auto',
@@ -12,45 +12,40 @@ export const dynamic = 'auto',
     preferredRegion = 'auto'
 ;
 
-async function getNotes() {
-    // const apiResponse = await fetch(
-    //     'http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30',
-    //     { cache: 'no-store' }
-    // );
-    // const dataJson = await apiResponse.json();
+async function getContacts() {
     const pocketBase = new PocketBase('http://127.0.0.1:8090');
-    const data = await pocketBase.collection('notes').getFullList({
+    const data = await pocketBase.collection('contacts').getFullList({
         sort: '-created', perPage: 30
     });
 
     return data as any[];
 }
 
-export default async function NotesPage() {
-    const notes = await getNotes();
+export default async function ContactsPage() {
+    const contacts = await getContacts();
 
     return (
         <div>
-            <h1>Notes</h1>
+            <h1>Contacts</h1>
 
             <div className={styles.grid}>
-                {notes?.map(note => {
-                    return <Note key={note.id} note={note} />;
+                {contacts?.map(contact => {
+                    return <Contact key={contact.id} contact={contact} />;
                 })}
             </div>
 
-            <CreateNote />
+            <CreateContact />
         </div>
     );
 }
 
-function Note({ note }: any) {
-    const { id, title, content, created } = note ||{};
+function Contact({ contact }: any) {
+    const { id, lastname, firstname, phone, created } = contact ||{};
     return (
-        <Link href={`/notes/${id}`}>
-            <div className={styles.note}>
-                <h2>{title}</h2>
-                <h5>{content}</h5>
+        <Link href={`/contacts/${id}`}>
+            <div className={styles.contact}>
+                <h2>{lastname} {firstname}</h2>
+                <h5>{phone}</h5>
                 <p>{created}</p>
             </div>
         </Link>
