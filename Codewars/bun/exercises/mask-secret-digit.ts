caf4c9574fae6994a23dc0760b133df16a8548c2
craftidev <ivann.codev@gmail.com>
2024-08-01 22:47:41 +0200

    ts - mask digit longer than 4

--- Codewars/bun/exercises/mask-secret-digit.ts
+++ Codewars/bun/exercises/mask-secret-digit.ts
@@ -1,7 +1,7 @@
 /*
 Usually when you buy something, you're asked whether your credit card number,
-phone number or answer to your most secret question is still correct. However,
-since someone could look over your shoulder, you don't want that shown on your screen.
+phone number or answer to your most secret question is still correct.
+However, since someone could look over your shoulder, you don't want that shown on your screen.
 Instead, we mask it.
 
 Your task is to write a function maskify, which changes all but the last four characters into '#'.
@@ -17,16 +17,34 @@ Examples (input --> output):
 "Nananananananananananananananana Batman!" --> "####################################man!"
 */
 
-import { assert } from 'chai'
-
-describe('maskify', function () {
-    it('should work for some examples', function () {
-        assert.equal(maskify('4556364607935616'), '############5616')
-        assert.equal(maskify('1'), '1')
-        assert.equal(maskify('11111'), '#1111')
-    })
-})
-
 function maskify(cc: string): string {
-    return "";
+    const charLength: number = cc.length;
+
+    if (charLength <= 4) {
+        return cc;
+    } else {
+        const mask: string = '#'.repeat(charLength - 4);
+        const fourLastDigit: string = cc.slice(-4).toString();
+
+        return mask + fourLastDigit;
+    }
+}
+
+if (require.main === module) {
+    console.log("THIS IS A NORMAL RUN - to run tests: `bun mocha file-name.ts`");
+} else {
+    // Tests
+    const chai = require('chai');
+    const assert = chai.assert;
+    describe('maskify', function () {
+        it('should mask for more than 4 digit', function () {
+            assert.equal(maskify('4556364607935616'), '############5616');
+            assert.equal(maskify('11111'), '#1111');
+        });
+
+        it('should NOT mask for less than 4 digit', function () {
+            assert.equal(maskify('1'), '1');
+            assert.equal(maskify('1244'), '1244');
+        });
+    });
 }
