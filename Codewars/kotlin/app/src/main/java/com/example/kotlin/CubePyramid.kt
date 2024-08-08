8ab21fd67b1285afd89f350a0227314e18f7d52d
craftidev <ivann.codev@gmail.com>
2024-08-08 12:31:14 +0200

    finding n for m = n^3 + (n-1)^3 + ... + 1^3

--- Codewars/kotlin/app/src/main/java/com/example/kotlin/CubePyramid.kt
+++ Codewars/kotlin/app/src/main/java/com/example/kotlin/CubePyramid.kt
@@ -4,7 +4,7 @@ package com.example.kotlin
 Your task is to construct a building which will be a pile of n cubes.
 The cube at the bottom will have a volume of n^3,
 the cube above will have volume of (n-1)^3 and so on
-until the top which will have a volume of 13 1^3 13.
+until the top which will have a volume of 1^3.
 
 You are given the total volume m of the building.
 Being given m can you find the number n of cubes you will have to build?
@@ -19,6 +19,30 @@ findNb(1071225) --> 45
 findNb(91716553919377) --> -1
 */
 
+import kotlin.random.Random // For custom test
+
+fun findNb(volumeInput: Long): Long {
+    var numberOfFloors: Long = 0L
+    var volume: Long = volumeInput
+
+    while (volume > 0) {
+        numberOfFloors++
+        volume -= (numberOfFloors * numberOfFloors * numberOfFloors)
+    }
+
+    if (volume == 0L && numberOfFloors > 0) {
+        return numberOfFloors
+    }
+
+    return -1
+}
+
 fun main() {
-    println("Hello, World!")
+    // Custom test
+    val randomInt: Long = Random.nextInt(1, 10000).toLong()
+    var m: Long = 0L
+
+    for (i in 1..randomInt) {
+        m += (i * i * i)
+    }
 }
