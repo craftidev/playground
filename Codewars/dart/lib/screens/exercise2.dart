b44681278c4ea767ea09adf44b2f4f449d7914ee
craftidev <ivann.codev@gmail.com>
2024-08-08 15:58:16 +0200

    test display

---
+++ Codewars/dart/lib/screens/exercise2.dart
@@ -0,0 +1,18 @@
+// lib/screens/exercise2.dart
+import 'package:flutter/material.dart';
+
+class Exercise2 extends StatelessWidget {
+  const Exercise2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exercise 2'),
+      ),
+      body: const Center(
+        child: Text('This is Exercise 2'),
+      ),
+    );
+  }
+}
