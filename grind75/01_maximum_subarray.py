b3f7b41062ea8a8d24a19e0e7d7e04911acafbbc
craftidev <ivann.codev@gmail.com>
2024-08-17 09:03:39 +0200

    implement class, comment out O(n) solution to work on D&Q

--- grind75/01_maximum_subarray.py
+++ grind75/01_maximum_subarray.py
@@ -33,69 +33,74 @@ from typing import List
 import unittest
 import random
 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """
+        This function finds the subarray with the largest sum in a given list of integers.
+        It uses a dynamic programming approach with a time complexity of O(n).
 
-def maxSubArray(self, nums: List[int]) -> int:
-    """
-    This function finds the subarray with the largest sum in a given list of integers.
-    It uses a dynamic programming approach with a time complexity of O(n).
+        Parameters:
+        nums (List[int]): The input list of integers.
 
-    Parameters:
-    nums (List[int]): The input list of integers.
+        Returns:
+        int: The sum of the subarray with the largest sum.
+        """
 
-    Returns:
-    int: The sum of the subarray with the largest sum.
-    """
-##### O(n^2) - first solution
-    # if len(nums) > 1:
-    #     for i in range(1, len(nums)):
-    #         if sum(nums[i * -1:]) < 0:
-    #             return maxSubArray(self, nums[:i * -1])
-    #         elif sum(nums[:i]) < 0:
-    #             return maxSubArray(self, nums[i:])
-    #
-    # return sum(nums)
-#####
-    # Rework for a big O complexity of O(n)
-    current_sum_max_subarray: int = -105 # reboot value
-    negative_streak_val: int = 0
-    stored_sum_max_subarray: int = -104 # min value
+        return 0
 
-    for element in nums:
-        # Init search
-        if current_sum_max_subarray == -105:
-            current_sum_max_subarray = element
-            continue
+    ##### O(n^2) - first solution
+        # if len(nums) > 1:
+        #     for i in range(1, len(nums)):
+        #         if sum(nums[i * -1:]) < 0:
+        #             return self.maxSubArray(nums[:i * -1])
+        #         elif sum(nums[:i]) < 0:
+        #             return self.maxSubArray(nums[i:])
+        #
+        # return sum(nums)
+    #####
 
-        # Find first positive number, or better negative number
-        if current_sum_max_subarray < 0 and current_sum_max_subarray < element:
-            current_sum_max_subarray = element
-            continue
-
-        # Positive encounter
-        if element >= 0:
-            # after a negative streak
-            if negative_streak_val < 0:
-                negative_streak_val += element
-                # element bigger than negative streak
-                if negative_streak_val > 0:
-                    current_sum_max_subarray += negative_streak_val
-                    negative_streak_val = 0
-            # after a positive streak
-            else:
-                current_sum_max_subarray += element
-        # Negative encounter
-        else:
-            negative_streak_val += element
-            # negative streak is getting too big
-            if negative_streak_val + current_sum_max_subarray <= 0:
-                # store potential best subarray
-                if stored_sum_max_subarray < current_sum_max_subarray:
-                    stored_sum_max_subarray = current_sum_max_subarray
-                    current_sum_max_subarray = -105
-                    negative_streak_val = 0
-
-    return max(current_sum_max_subarray, stored_sum_max_subarray)
-
+    #####
+        # First rework for a big O complexity of O(n)
+        # current_sum_max_subarray: int = -105 # reboot value
+        # negative_streak_val: int = 0
+        # stored_sum_max_subarray: int = -104 # min value
+        #
+        # for element in nums:
+        #     # Init search
+        #     if current_sum_max_subarray == -105:
+        #         current_sum_max_subarray = element
+        #         continue
+        #
+        #     # Find first positive number, or better negative number
+        #     if current_sum_max_subarray < 0 and current_sum_max_subarray < element:
+        #         current_sum_max_subarray = element
+        #         continue
+        #
+        #     # Positive encounter
+        #     if element >= 0:
+        #         # after a negative streak
+        #         if negative_streak_val < 0:
+        #             negative_streak_val += element
+        #             # element bigger than negative streak
+        #             if negative_streak_val > 0:
+        #                 current_sum_max_subarray += negative_streak_val
+        #                 negative_streak_val = 0
+        #         # after a positive streak
+        #         else:
+        #             current_sum_max_subarray += element
+        #     # Negative encounter
+        #     else:
+        #         negative_streak_val += element
+        #         # negative streak is getting too big
+        #         if negative_streak_val + current_sum_max_subarray <= 0:
+        #             # store potential best subarray
+        #             if stored_sum_max_subarray < current_sum_max_subarray:
+        #                 stored_sum_max_subarray = current_sum_max_subarray
+        #                 current_sum_max_subarray = -105
+        #                 negative_streak_val = 0
+        #
+        # return max(current_sum_max_subarray, stored_sum_max_subarray)
+    #####
 # Testing
 class TestMaxSubArray(unittest.TestCase):
     def test_only_negative_int(self):
